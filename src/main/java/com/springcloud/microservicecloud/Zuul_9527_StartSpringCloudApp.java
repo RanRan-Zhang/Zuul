5faab76ea0bc066ad58e36b4880969b4b1c18dc3
ranran01 <969634214@qq.com>
2021-05-13 01:15:55 +0800

    我再次修改了主启动类

--- src/main/java/com/springcloud/microservicecloud/Zuul_9527_StartSpringCloudApp.java
+++ src/main/java/com/springcloud/microservicecloud/Zuul_9527_StartSpringCloudApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy //开启网关代理，适用于网关
+@EnableZuulProxy //开启网关代理，适用于网关Zull网关
 public class Zuul_9527_StartSpringCloudApp {
     public static void main(String[] args) {
         SpringApplication.run(Zuul_9527_StartSpringCloudApp.class,args);
